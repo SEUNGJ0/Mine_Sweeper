559db7f21fb3569be84016c0c2c9129ad4c558c6
SEUNGJ0 <kuksj0312@naver.com>
2023-12-28 14:49:31 +0900

    에러 수정 1. 지뢰의 행렬값의 변화에 따른 초기화 오류 수정 2. 지뢰의 행렬값 범위의 제한 설정

--- MineLogic.py
+++ MineLogic.py
@@ -1,6 +1,6 @@
 from random import *
 
-def MainLogic(row, col, per):
+def generate_minesweeper_board(row, col, per):
     # 인덱스 생성 및 지뢰
     Mine_Info = {}
     t = [[1,0],[-1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]
@@ -8,25 +8,28 @@ def MainLogic(row, col, per):
     Mine_spot = []
     for i in range(row):
         for j in range(col):
-            Mine_Info[f"Index_{i}_{j}"] = {'row':i,'col':j,'Mine': False,'Around_Mine_num':0,'Pct':None}
+            Index = f"Index_{i}_{j}"
+            Mine_Info[Index] = {'row':i,'col':j,'Mine': False,'Around_Mine_num':0,'Pct':None}
             if randint(1,10) <= per:
-                Mine_Info[f"Index_{i}_{j}"].update(Mine = True) 
+                Mine_Info[Index]['Mine'] = True
                 Mine_spot.append([i,j]) 
                 # 지뢰 개수 카운트
                 Mine_count += 1
     
     for i in range(row):
         for j in range(col):
+            Index = f"Index_{i}_{j}"
             A_Mine_count = 0
-            if Mine_Info[f"Index_{i}_{j}"]['Mine'] == False:
-                Mine_Info[f"Index_{i}_{j}"].update({'Pct':randint(1,10)})
+            if not Mine_Info[Index]['Mine'] :
+                Mine_Info[Index]['Pct'] = randint(1,10)
                 for v in range(8):
                     if [i+t[v][0],j+t[v][1]] in Mine_spot:
                         A_Mine_count += 1
-            Mine_Info[f"Index_{i}_{j}"].update({'Around_Mine_num' : A_Mine_count}) 
+            Mine_Info[Index]['Around_Mine_num'] = A_Mine_count
+
     return Mine_Info, Mine_count, t
 
-def randindex(row, col):
+def generate_random_indexes(row, col):
     random_list = []
     for x in range(randint(2,round(row/3))):
         for y in range(randint(2,round(col/3))):
--- test/Mine_Sweeper_ver1.py
+++ test/Mine_Sweeper_ver1.py
@@ -1,25 +1,63 @@
 import tkinter as tk
-from tkinter import *
 from random import *
+from tkinter import *
 from functools import partial
-from MineLogic import generate_minesweeper_board, generate_random_indexes
+
+def Logic(row, col, per):
+    # 인덱스 생성 및 지뢰
+    Mine_Info = {}
+    t = [[1,0],[-1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]
+    Mine_count = 0
+    Mine_spot = []
+    for i in range(row):
+        for j in range(col):
+            Mine_Info[f"Index_{i}_{j}"] = {'row':i,'col':j,'Mine': False,'Around_Mine_num':0,'Pct':None}
+            if randint(1,10) <= per:
+                Mine_Info[f"Index_{i}_{j}"].update(Mine = True) 
+                Mine_spot.append([i,j]) 
+                # 지뢰 개수 카운트
+                Mine_count += 1
+    
+    for i in range(row):
+        for j in range(col):
+            A_Mine_count = 0
+            if Mine_Info[f"Index_{i}_{j}"]['Mine'] == False:
+                Mine_Info[f"Index_{i}_{j}"].update({'Pct':randint(1,10)})
+                for v in range(8):
+                    if [i+t[v][0],j+t[v][1]] in Mine_spot:
+                        A_Mine_count += 1
+            Mine_Info[f"Index_{i}_{j}"].update({'Around_Mine_num' : A_Mine_count}) 
+    return Mine_Info, Mine_count, t
+
+def randindex(row, col):
+    random_list = []
+    for x in range(randint(2,round(row/3))):
+        for y in range(randint(2,round(col/3))):
+            random_list.append([x,y])
+        for y in range(randint(2,round(col/3))):
+            random_list.append([x,-y])
+    for x in range(1,randint(2,round(row/3))):
+        for y in range(randint(2,round(col/3))):
+            random_list.append([-x,y])
+        for y in range(randint(2,round(col/3))):
+            random_list.append([-x,-y])
+    return random_list
 
 
-# tkinter root instance 생성
-root = tk.Tk()
-root.title("Mine Sweeper")
+root = Tk()
+root.title("Mine Sweeper")                   # 타이틀 제목 설정
+
+## 창 크기 설정 ##
 Wid_size = [900,600]
-Center_X = int(root.winfo_screenwidth()/2-Wid_size[0]/2)
-Center_Y = int(root.winfo_screenheight()/2-Wid_size[1]/2)
-root.geometry(f"{Wid_size[0]}x{Wid_size[1]}+{Center_X}+{Center_Y}")  # 가로 X 세로 + X좌표 + Y좌표 설정
+Center_X = int(root.winfo_screenwidth())/2-Wid_size[0]/2
+Center_Y = int(root.winfo_screenheight())/2-Wid_size[1]/2
+root.geometry("%dx%d+%d+%d"%(Wid_size[0],Wid_size[1],Center_X, Center_Y))  # 가로 X 세로 + X좌표 + Y좌표 설정
 root.resizable(False, False)  
 
-# Frame 생성
-frame1 = tk.Frame(root, relief="solid", bd=2, padx=20, pady=20)
-frame1.pack(side="top", pady=30)
-
-frame2 = tk.Frame(root, relief="solid", bd=2, padx=30, pady=30)
-frame2.pack(side="bottom", pady=30)
+frame1=tk.Frame(root, relief= SOLID, bd=2,padx=20, pady=20)
+frame1.pack(side = TOP,pady=30)
+frame2 = tk.Frame(root, relief= SOLID, bd=2,padx=30, pady=30)
+frame2.pack(side = BOTTOM,pady=30)
 
 ## 기본 행 열 값 설정 ##
 row, col = 5, 5
@@ -27,11 +65,6 @@ Row_Int = tk.IntVar()
 Column_Int = tk.IntVar()
 MSG_Txt = tk.StringVar()
 MSG = tk.Message(root, textvariable=MSG_Txt, width = 500).pack()
-# 최소값과 최대값 설정
-MIN_ROW = 5
-MAX_ROW = 10
-MIN_COL = 5
-MAX_COL = 19
 ## 행/열 설정 라벨 ## 
 tk.Label(frame1,text = "Row").grid(row = 1,column = 0) 
 tk.Label(frame1,text = "Column").grid(row = 2,column = 0)
@@ -53,7 +86,7 @@ Button(frame1,text = "START",command= lambda : Click('Start')).grid(row = 0,colu
 def setbutton(row = row, col = col):
     if row > 5:
         Button(frame1,text = "▼",command= lambda : Click('row_down')).grid(row = 1,column = 3)
-    elif row < 11:
+    elif row < 12:
         Button(frame1,text = "▲",command= lambda : Click('row_up')).grid(row = 1,column = 2)
     if col > 5:
         Button(frame1,text = "▼",command= lambda : Click('col_down')).grid(row = 2,column = 3)
@@ -62,13 +95,12 @@ def setbutton(row = row, col = col):
 
 def Matrix(row, col):
     global frame2
-    # frame2 초기화
     frame2.destroy()
     frame2 = tk.Frame(root, relief= SOLID, bd=2,padx=30, pady=30)
     frame2.pack(side = BOTTOM,pady=30)
 
-    Mine_Info, Mine_count , t= generate_minesweeper_board(row, col, 4)
-    random_list = generate_random_indexes(row, col)
+    Mine_Info, Mine_count , t= Logic(row, col, 4)
+    random_list = randindex(row, col)
     First_Click = True     # 첫 클릭인지를 확인 하는 변수
     Mistake_Count = 0   # 지뢰 클릭 횟수를 카운트하는 변수
 
@@ -77,7 +109,7 @@ def Matrix(row, col):
     tk.Label(frame1, text = f"총 지뢰 개수 : {Mine_count}").grid(row=0,column=4)
     MSG_Txt.set("지뢰찾기를 시작합니다.")
     
-    IndexButton = [[Button(frame2, relief = SOLID, borderwidth = 1, text = " ", width = 1) for j in range(col)] for i in range(row)]
+    IndexButton = [[Button(frame2, relief = SOLID, borderwidth = 1, text = " ", width = 2) for j in range(col)] for i in range(row)]
 
     for i in range(row):
         for j in range(col):
@@ -137,18 +169,18 @@ def Matrix(row, col):
 
 def Click(i):
     global row, col, Row_Int, Column_Int
-    if i == 'row_up' and row < MAX_ROW:
+    if i == 'row_up':
         row += 1
-    elif i == 'row_down' and row > MIN_ROW:
+    elif i == 'row_down': 
         row -= 1
-    elif i == 'col_up' and col < MAX_COL:
+    elif i == 'col_up':
         col += 1
-    elif i == 'col_down' and col > MIN_COL:
+    elif i == 'col_down': 
         col -= 1
     elif i == 'Start' and row > 0 and col > 0:
         row = Row_Int.get()
         col = Column_Int.get()
-
+    
     Ent_Row.delete(0,'end')
     Ent_Row.insert(0,row)
     Ent_Col.delete(0,'end')
